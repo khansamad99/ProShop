12d9c061e91eff5af41b9b1bde023f486050fa78
Samad Khan <samadk619@gmail.com>
2020-12-03 20:45:44 +0530

    HomeScreen created

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,12 +1,14 @@
 import {Container} from "react-bootstrap";
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Homescreen from './pages/Homescreen';
 function App() {
   return (
     <div>
       <Header/>
         <Container>
           <main className="py-3"><h1>Welcome To ProShop </h1></main>
+          <Homescreen/>
         </Container>
       <Footer/>
     </div>
