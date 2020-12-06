1ba1a7ea3f44ed85e786d0fc2f99b588c989abad
Samad Khan <samadk619@gmail.com>
2020-12-06 12:30:38 +0530

    env setup

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const dotenv = require('dotenv')
 const products = require('./data/products');
 
 const app = express();
+
+dotenv.config();
 
 app.get('/',(req,res) => {
     res.send('API is running...');
@@ -15,4 +18,6 @@ app.get('/api/product/:id',(req,res) => {
     const product = products.find(p => p._id === req.params.id);
     res.json(product);
 })
-app.listen(5000,console.log('Server is running on 5000'));
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT,console.log(`Server is running in ${process.env.NODE_ENV} mode on ${PORT}`));
