b391d7aa9139d18a9105e10e3dab9d2e68760dd8
Samad Khan <samadk619@gmail.com>
2020-12-09 14:01:36 +0530

    Sample data script added

--- backend/datainput.js
+++ backend/datainput.js
@@ -35,3 +35,23 @@ const importData = async () => {
         process.exit(1)
     }
 }
+
+const destroyData = async () => {
+    try {
+      await Order.deleteMany()
+      await Product.deleteMany()
+      await User.deleteMany()
+  
+      console.log('Data Destroyed!'.red.inverse)
+      process.exit()
+    } catch (error) {
+      console.error(`${error}`.red.inverse)
+      process.exit(1)
+    }
+  }
+  
+  if (process.argv[2] === '-d') {
+    destroyData()
+  } else {
+    importData()
+  }
