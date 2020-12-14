7608d29766c317c1d01ef6107f63477c1ba4155d
Samad Khan <samadk619@gmail.com>
2020-12-14 23:34:15 +0530

    Product action

--- ui/src/store.js
+++ ui/src/store.js
@@ -1,8 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension';
+import {productReducers} from './reducers/productReducer';
 
-const reducer = combineReducers({});
+const reducer = combineReducers({
+    productList:productReducers
+});
 
 const initState = {};
 const middleware = [thunk];
