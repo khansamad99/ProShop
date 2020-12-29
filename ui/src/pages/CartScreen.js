3c255368524a2ac4b6aca4fd4810d7d33f5a722c
Samad Khan <samadk619@gmail.com>
2020-12-29 14:41:50 +0530

    Cart Screen Done

--- ui/src/pages/CartScreen.js
+++ ui/src/pages/CartScreen.js
@@ -21,6 +21,10 @@ const CartScreen = ({match,location,history}) => {
         }
     },[dispatch,productId,qty])
 
+    const checkoutHandler = () => {
+        history.push('/login?redirect=shipping')
+    }
+
     return (
         <Row>
             <Col md={8}>
@@ -31,7 +35,27 @@ const CartScreen = ({match,location,history}) => {
                         <ListGroup.Item key={item.product}>
                             <Row>
                                 <Col md={2}>
-                                    <Image src={item.image}/>
+                                    <Image src={item.image} alt={item.name} fluid rounded/>
+                                </Col>
+                                <Col md={3}>
+                                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                                </Col>
+                                <Col md={2}>${item.price}</Col>
+                                <Col md={2}>
+                                    <Form.Control style={{padding:'10px'}}as='select' value={item.qty} onChange={(e) => 
+                                        dispatch(addToCart(item.product,Number(e.target.value)))
+                                    }>
+                                    {[...Array(item.countInStock).keys()].map((x) => (
+                                        <option key={x+1} value={x+1}>
+                                            {x+1}
+                                        </option>
+                                    ))}
+                                    </Form.Control>
+                                </Col>
+                                <Col md={2}>
+                                    <Button type='button' variant='light'>
+                                        <i className='fas fa-trash'></i>
+                                    </Button>
                                 </Col>
                             </Row>
                         </ListGroup.Item>
@@ -39,12 +63,33 @@ const CartScreen = ({match,location,history}) => {
                 </ListGroup>
             )}
             </Col>
-            <Col md={2}>
+            <Col md={4}>
             
-            </Col>
-            <Col md={2}>
-            
-            </Col>
+            <Card>
+              <ListGroup variant='flush'>
+                <ListGroup.Item>
+                  <h2>
+                    Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
+                    items
+                  </h2>
+                  $
+                  {cartItems
+                    .reduce((acc, item) => acc + item.qty * item.price, 0)
+                    .toFixed(2)}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Button
+                    type='button'
+                    className='btn-block'
+                    disabled={cartItems.length === 0}
+                    onClick={checkoutHandler}
+                  >
+                    Proceed To Checkout
+                  </Button>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card>
+          </Col>
         </Row>
     )
 }
