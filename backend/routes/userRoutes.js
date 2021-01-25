ca7240204de4f780b202f12a88891d0bf456be87
Samad Khan <samadk619@gmail.com>
2021-01-25 13:09:21 +0530

    Update profile route added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const {authUser,getUserProfile,registerUser} = require('../controllers/userController');
+const {authUser,getUserProfile,registerUser,updateProfile} = require('../controllers/userController');
 const {protect} = require('../middlewares/authRoute');
 const router = express.Router();
 
 router.post('/',registerUser)
 router.post('/login', authUser)
-router.route('/profile').get(getUserProfile,protect)
-
+router.route('/profile')
+    .get(getUserProfile,protect)
+    .put(updateUserProfile,protect)
 module.exports = router;
