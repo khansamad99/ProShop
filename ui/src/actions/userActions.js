4dbb9132057df72980515956b6dea0d60969fec4
Samad Khan <samadk619@gmail.com>
2021-02-18 23:09:59 +0530

    Order Details and Order Reset

--- ui/src/actions/userActions.js
+++ ui/src/actions/userActions.js
@@ -11,8 +11,10 @@ import {
     USER_DETAILS_FAIL,
     USER_UPDATE_FAIL,
     USER_UPDATE_SUCCESS,
-    USER_UPDATE_REQUEST
+    USER_UPDATE_REQUEST,
+    USER_DETAILS_RESET
 } from '../constants/userConstants';
+import {ORDER_LIST_MY_RESET} from '../constants/orderConstants'
 import axios from "axios";
 export const login = (email,password) => async (dispatch) => {
     try {
@@ -46,6 +48,8 @@ export const login = (email,password) => async (dispatch) => {
 export const logout = () => (dispatch) => {
     localStorage.removeItem('userInfo')
     dispatch({ type: USER_LOGOUT })
+    dispatch({type:USER_DETAILS_RESET})
+    dispatch({type:ORDER_LIST_MY_RESET})
     document.location.href = '/login'
 }
 
