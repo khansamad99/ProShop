4b732d4f5598cb10e0665e3fb8a0e813adbeb914
Samad Khan <samadk619@gmail.com>
2021-02-20 21:49:56 +0530

    Get Admin Users

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -118,3 +118,11 @@ exports.updateUserProfile = asyncHandler(async (req, res) => {
     throw new Error('User not found')
   }
 })
+
+// @desc    Get all users
+// @route   GET /api/users
+// @access  Private/Admin
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({})
+  res.json(users)
+})
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const {authUser,getUserProfile,registerUser,updateUserProfile} = require('../controllers/userController');
-const {protect} = require('../middlewares/authRoute');
+const { getUserDetails } = require('../../ui/src/actions/userActions');
+const {authUser,getUserProfile,registerUser,updateUserProfile,getUsers} = require('../controllers/userController');
+const {protect, admin} = require('../middlewares/authRoute');
 const router = express.Router();
 
+router.route('/').post(registerUser).get(protect, admin, getUsers)
 router.post('/',registerUser)
 router.post('/login', authUser)
 router.route('/profile')
