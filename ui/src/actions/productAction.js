51b747450a19384469960cd1cc12d0fc360fd855
Samad Khan <samadk619@gmail.com>
2021-02-28 17:45:31 +0530

    Create and Delete Product by Admin

--- ui/src/actions/productAction.js
+++ ui/src/actions/productAction.js
@@ -79,3 +79,84 @@ export const deleteProduct = (id) => async (dispatch, getState) => {
     }
   }
   
+
+  export const createProduct = () => async (dispatch, getState) => {
+    try {
+      dispatch({
+        type: PRODUCT_CREATE_REQUEST,
+      })
+  
+      const {
+        userLogin: { userInfo },
+      } = getState()
+  
+      const config = {
+        headers: {
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      }
+  
+      const { data } = await axios.post(`/api/products`, {}, config)
+  
+      dispatch({
+        type: PRODUCT_CREATE_SUCCESS,
+        payload: data,
+      })
+    } catch (error) {
+      const message =
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+      if (message === 'Not authorized, token failed') {
+        dispatch(logout())
+      }
+      dispatch({
+        type: PRODUCT_CREATE_FAIL,
+        payload: message,
+      })
+    }
+  }
+  
+  export const updateProduct = (product) => async (dispatch, getState) => {
+    try {
+      dispatch({
+        type: PRODUCT_UPDATE_REQUEST,
+      })
+  
+      const {
+        userLogin: { userInfo },
+      } = getState()
+  
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      }
+  
+      const { data } = await axios.put(
+        `/api/products/${product._id}`,
+        product,
+        config
+      )
+  
+      dispatch({
+        type: PRODUCT_UPDATE_SUCCESS,
+        payload: data,
+      })
+      dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data })
+    } catch (error) {
+      const message =
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+      if (message === 'Not authorized, token failed') {
+        dispatch(logout())
+      }
+      dispatch({
+        type: PRODUCT_UPDATE_FAIL,
+        payload: message,
+      })
+    }
+  }
+  
--- ui/src/store.js
+++ ui/src/store.js
@@ -1,7 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension';
-import {productReducers,productDetailsReducers, productDeleteReducer} from './reducers/productReducer';
+import {productReducers,productDetailsReducers, productDeleteReducer, productCreateReducer, productUpdateReducer} from './reducers/productReducer';
 import {cartReducer} from './reducers/cartReducer';
 import {userLoginReducer,userRegisterReducer,userDetailsReducer,userUpdateProfileReducer,userListReducer, userDeleteReducer} from './reducers/userReducers';
 import {orderCreateReducer,orderDetailsReducer, orderListMyReducer, orderPayReducer} from './reducers/orderReducer';
@@ -22,6 +22,8 @@ const reducer = combineReducers({
     productList:productReducers,
     productDetails:productDetailsReducers,
     productDelete:productDeleteReducer,
+    productCreate: productCreateReducer,
+    productUpdate: productUpdateReducer,
     cart:cartReducer,
     userLogin:userLoginReducer,
     userRegister:userRegisterReducer,
