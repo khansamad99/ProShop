bf8f80170a2ab962070c014800568ba6ef53f7f9
Samad Khan <samadk619@gmail.com>
2021-03-03 12:28:41 +0530

    Upload Routes

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const products = require('./data/products');
@@ -6,6 +7,7 @@ const colors = require('colors');
 const productRoutes = require('./routes/productRoutes');
 const userRoutes = require('./routes/userRoutes');
 const orderRoutes = require('./routes/orderRoutes');
+const uploadRoutes = require('./routes/uploadRoutes');
 const app = express();
 
 connect();
@@ -20,9 +22,15 @@ app.use(express.json())
 app.use('/api/users',userRoutes)
 app.use('/api/products',productRoutes);
 app.use('/api/order',orderRoutes);
+app.use('/api/upload',uploadRoutes);
 app.use('/api/config/paypal',(req,res) => {
     res.send(process.env.CLIENT_ID)
 })
+
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT,console.log(`Server is running in ${process.env.NODE_ENV} mode on ${PORT}`.yellow.bold));
